cda64eaf66f4dc983d7513724478f9daff052704
Iván Rodríguez <ivanrodriguezmillan@outlook.com>
2022-09-08 12:27:15 +0200

    More examples with rectangles.

--- PrototypePattern/Program.cs
+++ PrototypePattern/Program.cs
@@ -16,9 +16,18 @@ internal class Program
         shapeRegistry.addShape("BigRectangle", bigRectangle);
         shapeRegistry.addShape("CopyBasicRectangle", copyBasicRectangle);
 
-        Rectangle? rectangleToShow = (Rectangle)shapeRegistry.getById("hola");
+        Rectangle? rectangleCopyToShow = (Rectangle)shapeRegistry.getById("CopyBasicRectangle");
 
-        if(rectangleToShow != null)
+        rectangleCopyToShow.modifyCoordinate(23, 41);
+
+        if (rectangleCopyToShow != null)
+            Console.WriteLine($"Coordinate X ---> {rectangleCopyToShow.getCoordinateX()} \nCoordinate Y ---> {rectangleCopyToShow.getCoordinateY()}");
+        else
+            Console.WriteLine($"There isn't coordinates in the shape because we dind't find anything.");
+
+        Rectangle? rectangleToShow = (Rectangle)shapeRegistry.getById("BasicRectangle");
+
+        if (rectangleToShow != null)
             Console.WriteLine($"Coordinate X ---> {rectangleToShow.getCoordinateX()} \nCoordinate Y ---> {rectangleToShow.getCoordinateY()}");
         else
             Console.WriteLine($"There isn't coordinates in the shape because we dind't find anything.");
